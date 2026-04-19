52dab4c0ba2eb4d4d11429a204192fc995c6cca5
Jonathyn-Winslow <winsloj4@my.erau.edu>
2026-04-19 16:03:25 -0700

    Add files via upload

---
+++ MazeGame.m
@@ -0,0 +1,116 @@
+maze1 = [5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+5 3 0 0 1 0 0 0 1 0 0 0 1 0 0 0 0 0 4 5
+5 1 1 0 1 0 1 0 1 0 1 0 1 1 1 1 0 1 0 5
+5 0 0 0 0 0 1 0 0 0 1 0 0 0 0 1 0 1 0 5
+5 0 1 1 1 0 1 1 1 0 1 1 1 1 0 1 0 1 0 5
+5 0 1 0 0 0 0 0 1 0 0 0 0 1 0 0 0 1 0 5
+5 0 1 0 1 1 1 0 1 1 1 1 0 1 1 1 0 1 0 5
+5 0 0 0 1 0 0 0 0 0 0 1 0 0 0 1 0 0 0 5
+5 1 1 0 1 0 1 1 1 1 0 1 1 1 0 1 1 1 0 5
+5 0 0 0 0 0 1 0 0 1 0 0 0 1 0 0 0 1 0 5
+5 0 1 1 1 0 1 0 0 1 1 1 0 1 1 1 0 1 0 5
+5 0 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 5
+5 0 1 0 1 1 1 1 1 1 0 1 1 1 1 1 0 1 0 5
+5 0 1 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 5
+5 0 1 1 1 1 1 1 0 1 1 1 1 1 0 1 1 1 0 5
+5 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 1 0 5
+5 1 1 1 1 1 0 1 1 1 1 1 0 1 1 1 0 1 0 5
+5 0 0 0 0 1 0 0 0 0 0 1 0 0 0 1 0 0 0 5
+5 1 1 1 0 1 1 1 1 1 0 1 1 1 0 1 1 1 9 5
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5];
+
+maze2 = [5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+5 6 0 0 0 1 0 0 0 0 1 0 0 0 0 0 0 0 0 5
+5 1 1 1 0 1 0 1 1 0 1 0 1 1 1 1 1 1 0 5
+5 3 0 0 0 1 0 0 1 0 0 0 0 0 0 0 0 1 0 5
+5 0 1 1 1 1 1 0 1 1 1 1 1 1 1 1 0 1 0 5
+5 0 0 0 0 0 1 0 0 0 0 0 0 0 0 1 0 1 0 5
+5 1 1 1 1 0 1 1 1 1 1 1 1 1 0 1 0 1 0 5
+5 0 0 0 1 0 0 0 0 0 0 0 0 1 0 1 0 0 0 5
+5 0 1 0 1 1 1 1 1 1 1 0 1 1 0 1 1 1 0 5
+5 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 0 5
+5 0 1 1 1 1 1 1 1 0 1 1 1 1 1 1 0 1 0 5
+5 0 0 0 0 0 0 0 1 0 0 0 0 0 0 1 0 1 0 5
+5 1 1 1 1 1 1 0 1 1 1 1 1 1 0 1 0 1 0 5
+5 0 0 0 0 0 1 0 0 0 0 0 0 1 0 0 0 1 0 5
+5 0 1 1 1 0 1 1 1 1 1 1 0 1 1 1 1 1 0 5
+5 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 0 1 0 5
+5 1 1 0 1 1 1 1 1 1 0 1 1 1 1 1 0 1 0 5
+5 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 4 0 5
+5 0 1 1 1 1 1 1 0 1 1 1 1 1 0 1 1 1 0 5
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5];
+
+maze3 = [5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5
+5 3 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 5
+5 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 4 1 5
+5 2 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 5
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5];
+
+% All of the mazes above were placed into a 2D array to create the game map for our maze
+
+writematrix(maze1, 'maze1.txt', 'Delimiter', 'space');
+writematrix(maze2, 'maze2.txt', 'Delimiter', 'space');
+writematrix(maze3, 'maze3.txt', 'Delimiter', 'space');
+
+%We stored all the mazes into txt files so it can be reloaded and used in other programs
+
+
+
+
+subplot(1,3,1)
+imagesc(maze1) % this converts the matrix values into pixels
+% imagesc was found in the MATLAB webside to convert the numbers into pixels for the maze
+caxis([0 9]);
+% caxis is the most important line of code, ensuring that each number gets its own color value, it acts as a kind of ruler to ensure that all values 0 to 9 gets the same color and makes the mazes consistent
+% caxis was found from ChatGPT to ensure each square gets its own assigned color
+axis equal tight % this ensures that the squares stay squared and removes extra padding to ensure that the maze looks like a proper grid
+% 'axis equal tight' was from ChatGPT while debugging because the maze wasn't in proper grid form
+title('Maze 1')
+
+subplot(1,3,2)
+imagesc(maze2)
+caxis([0 9]);
+axis equal tight
+title('Maze 2')
+
+subplot(1,3,3)
+imagesc(maze3)
+caxis([0 9]);
+axis equal tight
+title('Maze 3')
+
+
+
+
+% the RGB triplits are used to make specific colors
+colormap([
+0 0 0; % 1. Black
+1 1 1; % 2. White
+1 1 0; % 3. Yellow
+1 0 0; % 4. Red
+0 0 1; % 5. Blue
+0.5 0.5 0.5; % 6. Gray
+0 0 0; % 7. Black
+0 0 0; % 8. Black
+0 0 0; % 9. Black
+0 1 0; % 10. Green
+]);
+
+% This colormap allows colors to be assigned to each integer in the maze
+% We determined the colormap color variations through the MATLAB website
+% The caxis integration into the program was gotten via ChatGPT, including the method of assigning the color values 0-9
